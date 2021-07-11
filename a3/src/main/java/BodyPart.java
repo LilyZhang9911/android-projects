c759b45d6a24a1546488edb69f21b210404b4933
Jiawen Lily Zhang <jl6zhang@uwaterloo.ca>
2021-07-11 17:21:55 -0400

    added rotation

--- a3/src/main/java/BodyPart.java
+++ a3/src/main/java/BodyPart.java
@@ -1,81 +1,336 @@
 import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
+
 import java.util.Vector;
 
 
 public class BodyPart extends ImageView {
-    private ImageView img;
+    private static double HEAD_THETA_MAX = 50;
+    private static double LOWER_ARM_THETA_LEFT_BOUND = 135;
+    private static double LOWER_ARM_THETA_RIGHT_BOUND = 225;
+    private static double HAND_THETA_LEFT_BOUND = 35; // also used for feet with 35 degree limit
+    private static double HAND_THETA_RIGHT_BOUND = 325;
+    private static double LEG_THETA_LEFT_BOUND =90;
+    private static double LEG_THETA_RIGHT_BOUND = 270;
+
+    // note upper arm has no limit
+    private double rotation_pivot_x, rotation_pivot_y;
+    private ImageView imgview;
+    private Image img;
     private double initial_x, initial_y;
-    private double prev_x, prev_y;
-    enum STATE {NONE, SELECTED, MOVING}
+    enum PART {BODY, HEAD, UPPER_ARM, LOWER_ARM, HAND, UPPER_LEG, LOWER_LEG, FOOT}
+    enum ROTATE_DIR {LEFT, RIGHT, UP, DOWN}
     String name; // for debugging purposes
-    STATE cur_state;
+    PART cur_part;
+    private Affine matrix;
+    private Affine translate_matrix;
+    private Affine rotate_matrix;
+    BodyPart parent;
     protected Vector<BodyPart> children;
-    public BodyPart(Image i, double x, double y, String name) {
-        img = new ImageView(i);
-        img.setX(x);
-        img.setY(y);
+    double total_theta;
+    public BodyPart(Image i, double x, double y, PART part, String name) {
+        imgview = new ImageView(i);
+        img = i;
         initial_x = x;
         initial_y = y;
-        prev_x = -1;
-        prev_y = -1;
+        cur_part = part;
         this.name = name;
+        parent = null;
         children = new Vector<BodyPart> ();
+        total_theta = 0;
+        translate_matrix = new Affine();
+        translate_matrix.prependTranslation(x, y);
+        rotate_matrix = new Affine();
+        matrix = new Affine();
 
-        img.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                prev_x = -1;
-                prev_y = -1;
-                System.out.println(name + " pressed");
-                cur_state = STATE.SELECTED;
-            }
-        });
+        if (part == PART.HEAD) {
+            rotation_pivot_x = 0; // pivot is image size / 2 for head
+            rotation_pivot_y = 0;
+        }
+        //rotate_matrix.prependRotation(90);
 
-        img.addEventHandler(MouseEvent.MOUSE_DRAGGED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (prev_x == -1) prev_x = event.getX();
-                if (prev_y == -1) prev_y = event.getY();
+        update_matrix();
+    }
 
-                double dx = event.getX() - prev_x;
-                double dy = event.getY() - prev_y;
-
-                System.out.println(name + " dragged");
-                System.out.println(dx);
-                System.out.println(dy);
-                translate (dx, dy);
-                cur_state = STATE.MOVING;
-                // change for moving
-                prev_x = event.getX();
-                prev_y = event.getY();
-            }
-        });
-
-        img.addEventHandler(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                cur_state = STATE.NONE;
-            }
-        });
+    public BodyPart(Image i, double x, double y, PART part, String name, double pivot_x, double pivot_y) {
+        imgview = new ImageView(i);
+        img = i;
+        initial_x = x;
+        initial_y = y;
+        cur_part = part;
+        this.name = name;
+        parent = null;
+        children = new Vector<BodyPart> ();
+        total_theta = 0;
+        translate_matrix = new Affine();
+        translate_matrix.prependTranslation(x, y);
+        rotate_matrix = new Affine();
+        matrix = new Affine();
+        rotation_pivot_x = pivot_x; // pivot is image size / 2 for head
+        rotation_pivot_y = pivot_y;
+        update_matrix();
     }
 
     public void add_children (BodyPart c) {
         children.add(c);
+        c.parent = this;
     }
 
-    public ImageView get_img () {
-        return img;
+    // combines translate and rotate
+    public void update_matrix() {
+        matrix.setToIdentity(); // reset
+        // apply in order translate then rotate
+        matrix.append(rotate_matrix);
+        matrix.append(translate_matrix);
     }
 
-    public void translate (double dx, double dy) {
-        img.setX(img.getX() + dx);
-        img.setY(img.getY() + dy);
-        // move children
+    public void draw (GraphicsContext gc) {
+        //System.out.println("drawing "  + name);
+        Affine oldMatrix = gc.getTransform();
+        gc.setTransform(getFullMatrix());
+        gc.drawImage(img, 0, 0);
+        int children_count = children.size();
+        for (int i = children_count - 1; i >= 0; i--) {
+            children.get(i).draw(gc);
+        }
+        gc.setTransform(oldMatrix);
+    }
+
+    public Affine getFullMatrix() {
+        Affine result = new Affine();
+        // append all translations then rotations
+        Affine translate = getFullTranslateMatrix();
+        Affine rotate = getFullRotateMatrix();
+        result.append(rotate);
+        result.append(translate);
+
+        return result;
+    }
+
+    /*
+    public Affine getLocalMatrix(){
+        update_rotate();
+        update_matrix();
+        return matrix;
+    } */
+
+    public Affine getFullTranslateMatrix() {
+        Affine fullMatrix = getLocalTranslateMatrix().clone();
+        if (parent != null) {
+            fullMatrix.prepend(parent.getFullTranslateMatrix());
+        }
+        return fullMatrix;
+    }
+
+    public Affine getLocalTranslateMatrix() {
+        return translate_matrix;
+    }
+
+    public Affine getFullRotateMatrix() {
+        Affine fullMatrix = getLocalRotateMatrix().clone();
+        if (parent != null) {
+            fullMatrix.prepend(parent.getFullRotateMatrix());
+        }
+        return fullMatrix;
+    }
+
+    public Affine getLocalRotateMatrix() {
+        update_rotate();;
+        return rotate_matrix;
+    }
+
+    protected BodyPart hitTest (double x, double y) {
+        Point2D pointAtOrigin = new Point2D(0, 0);
+        try {
+            Point2D mouse_click = new Point2D(x, y);
+            pointAtOrigin = getFullMatrix().createInverse().transform(mouse_click);
+
+        } catch (NonInvertibleTransformException e) {
+            e.printStackTrace();
+        }
+
+        BodyPart result;
         for (BodyPart bp: children) {
-            bp.translate(dx, dy);
+            result = bp.hitTest(x, y);
+            if (result != null) return result;
+        }
+        if (imgview.contains(pointAtOrigin.getX(), pointAtOrigin.getY())) return this;
+        return null;
+    }
+
+    public void process_move (double dx, double dy, double theta, ROTATE_DIR x_dir, ROTATE_DIR y_dir) {
+        if (cur_part == PART.BODY) {
+            translate(dx, dy);
+        } else if (cur_part == PART.HEAD) {
+            rotate_head (theta, x_dir);
+        } else if (cur_part == PART.UPPER_ARM || cur_part == PART.LOWER_ARM ||
+                cur_part == PART.HAND || cur_part == PART.UPPER_LEG ||
+                cur_part == PART.LOWER_LEG || cur_part == PART.FOOT) {
+            rotate_arm (theta, x_dir, y_dir);
+        }
+    }
+
+    private void translate (double dx, double dy) {
+        translate_matrix.prependTranslation(dx, dy);
+    }
+
+    private void update_rotate() {
+        Affine fullMatrix = getFullTranslateMatrix();
+        Affine inverse = new Affine();
+        try {
+            inverse = fullMatrix.createInverse();
+        } catch (NonInvertibleTransformException e) {
+            e.printStackTrace();
+        }
+        rotate_matrix.setToIdentity();
+        rotate_matrix.prepend(inverse);
+        rotate_matrix.prependTranslation(-rotation_pivot_x, -rotation_pivot_y); // use bottom centre of head as pivot
+        rotate_matrix.prependRotation(total_theta);
+        rotate_matrix.prependTranslation(rotation_pivot_x, rotation_pivot_y);
+        rotate_matrix.prepend(fullMatrix);
+        update_matrix();
+    }
+
+    private void rotate_head (double theta, ROTATE_DIR dir) {
+        double limit = 0;
+        limit = HEAD_THETA_MAX;
+        // determine direction
+        if (dir == ROTATE_DIR.RIGHT) {
+            theta = -theta;
+        }
+        double remaining; // remaining degrees to rotate by
+        if (dir == ROTATE_DIR.RIGHT) {
+            remaining = - limit - total_theta;
+        } else {
+            remaining = limit - total_theta;
+        }
+        if (dir == ROTATE_DIR.LEFT && theta > remaining) {
+            total_theta = limit;
+        } else if (dir == ROTATE_DIR.RIGHT && theta < remaining) {
+            total_theta = -limit;
+        } else {
+            total_theta += theta;
+        }
+    }
+
+    private void rotate_arm (double theta, ROTATE_DIR x_dir, ROTATE_DIR y_dir) {
+        double parent_theta = 0;
+         if (cur_part == PART.LOWER_ARM) {
+             parent_theta = parent.total_theta;
+         } else if (cur_part == PART.HAND) {
+             parent_theta = parent.total_theta + parent.parent.total_theta;
+         } else if (cur_part == PART.LOWER_LEG) {
+             parent_theta = parent.total_theta;
+         } else if (cur_part == PART.FOOT) {
+             parent_theta = parent.total_theta + parent.parent.total_theta;
+         }
+
+        theta = Math.abs(theta);
+        //System.out.println("theta is " + theta);
+        int case_type = 0; // determine type of rotation
+        ROTATE_DIR total_dir;
+        if (x_dir == ROTATE_DIR.LEFT && y_dir == ROTATE_DIR.DOWN) {
+            case_type = 1;
+        } else if (x_dir == ROTATE_DIR.LEFT && y_dir == ROTATE_DIR.UP) {
+            case_type = 2;
+        } else if (x_dir == ROTATE_DIR.RIGHT && y_dir == ROTATE_DIR.DOWN) {
+            case_type = 3;
+        } else { // (x_dir == ROTATE_DIR.RIGHT && y_dir == ROTATE_DIR.UP)
+            case_type = 4;
+        }
+
+        // set theta in range 0 to 360
+        total_theta += 360;
+        total_theta %= 360;
+
+        double quadrant_theta = total_theta + parent_theta;
+        quadrant_theta += 360;
+        quadrant_theta %= 360;
+    //    System.out.println("quadrant theta is " + quadrant_theta);
+
+        if (quadrant_theta < 45) {
+         //   System.out.println("45 type " + case_type);
+            if (case_type == 1 || case_type == 2) {
+                theta = -theta;
+            }
+        } else if (quadrant_theta < 90) {
+           // System.out.println("90 type " + case_type);
+            if (case_type == 1 || case_type == 3) {
+                theta = -theta;
+            }
+        } else if (quadrant_theta < 135) {
+          //  System.out.println("135 type " + case_type);
+            if (case_type == 1 || case_type == 3) {
+                theta = -theta;
+            }
+        } else if (quadrant_theta < 180) {
+          //  System.out.println("180 type " + case_type);
+            if (case_type == 3 || case_type == 4) {
+                theta = -theta;
+            }
+        } else if (quadrant_theta < 225) {
+       //     System.out.println("225 type " + case_type);
+            if (case_type == 3 || case_type == 4) {
+                theta = -theta;
+            }
+        }
+        else if (quadrant_theta < 270) {
+          //  System.out.println("270 type " + case_type);
+            if (case_type == 2|| case_type == 3 || case_type == 4) {
+                theta = -theta;
+            }
+        } else if (quadrant_theta < 315) {
+          //  System.out.println("315 type " + case_type);
+            if (case_type == 2 || case_type == 4) {
+                theta = -theta;
+            }
+        } else { // quadrant_theta < 360
+          //  System.out.println("360 type " + case_type);
+            if (case_type == 1 || case_type == 2) {
+                theta = -theta;
+            }
+        }
+        total_theta += theta;
+        // Note: upper arm has not rotation limit
+        // adjustments for rotation limits below
+        if (cur_part == PART.LOWER_ARM) {
+            if (total_theta < 180 && total_theta > LOWER_ARM_THETA_LEFT_BOUND) {
+                total_theta = LOWER_ARM_THETA_LEFT_BOUND;
+            } else if (total_theta > 180 && total_theta < LOWER_ARM_THETA_RIGHT_BOUND) {
+                total_theta = LOWER_ARM_THETA_RIGHT_BOUND;
+            }
+        } else if (cur_part == PART.HAND || cur_part == PART.FOOT) {
+            if (total_theta < 180 && total_theta > HAND_THETA_LEFT_BOUND) {
+                total_theta = HAND_THETA_LEFT_BOUND;
+            } else if (total_theta > 180 && total_theta < HAND_THETA_RIGHT_BOUND) {
+                total_theta = HAND_THETA_RIGHT_BOUND;
+            }
+        } else if (cur_part == PART.UPPER_LEG || cur_part == PART.LOWER_LEG) {
+            if (total_theta < 180 && total_theta > LEG_THETA_LEFT_BOUND) {
+                total_theta = LEG_THETA_LEFT_BOUND;
+            } else if (total_theta > 180 && total_theta < LEG_THETA_RIGHT_BOUND) {
+                total_theta = LEG_THETA_RIGHT_BOUND;
+            }
+        }
+
+    }
+
+    public void reset() {
+        matrix = new Affine();
+        total_theta = 0;
+        translate_matrix.setToIdentity();
+        translate_matrix.prependTranslation(initial_x, initial_y);
+        rotate_matrix.setToIdentity();
+        update_matrix();
+        for (BodyPart bp: children) {
+            bp.reset();
         }
     }
 
--- a3/src/main/java/Ragdoll.java
+++ a3/src/main/java/Ragdoll.java
@@ -1,60 +1,80 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
-public class Ragdoll extends Application{
+import javax.swing.plaf.basic.BasicBorders;
+import java.awt.*;
 
-    void init_robin (Image i_head, Image i_body, Image i_lua, Image i_lla, Image i_lh,
+public class Ragdoll extends Application{
+    final int screen_width = 1024;
+    final int screen_height = 1280;
+    double prev_x, prev_y;
+    BodyPart root = null;
+    Canvas canvas;
+    BodyPart selected = null;
+
+    BodyPart init_robin (Image i_head, Image i_body, Image i_lua, Image i_lla, Image i_lh,
                      Image i_rua, Image i_rla, Image i_rh,
                      Image i_lul, Image i_lll, Image i_lf,
-                     Image i_rul, Image i_rll, Image i_rf,
-                     Pane robin_pane) {
-        BodyPart body = new BodyPart(i_body, 425, 400, "body");
-        BodyPart head = new BodyPart(i_head, 410, 240, "head");
-        BodyPart lua = new BodyPart(i_lua, 405, 410, "left upper arm");
-        BodyPart lla = new BodyPart(i_lla, 354, 488, "left lower arm");
-        BodyPart lh = new BodyPart(i_lh, 280, 487, "left hand");
+                     Image i_rul, Image i_rll, Image i_rf
+                     //Pane robin_pane
+                     ) {
+       /* BodyPart body = new BodyPart(i_body, 425, 400, BodyPart.PART.BODY, "body");
+        BodyPart head = new BodyPart(i_head, 410, 240, BodyPart.PART.HEAD,  "head");
 
-        BodyPart rua = new BodyPart(i_rua, 545, 420, "right upper arm");
-        BodyPart rla = new BodyPart(i_rla, 560, 533, "right lower arm");
-        BodyPart rh = new BodyPart(i_rh, 556, 605, "right hand");
+        BodyPart lua = new BodyPart(i_lua, 405, 410, BodyPart.PART.HEAD,  "left upper arm");
+        BodyPart lla = new BodyPart(i_lla, 354, 488, BodyPart.PART.HEAD,  "left lower arm");
+        BodyPart lh = new BodyPart(i_lh, 280, 487, BodyPart.PART.HEAD,  "left hand");
 
-        BodyPart lul = new BodyPart(i_lul, 405, 593, "left upper leg");
-        BodyPart lll = new BodyPart(i_lll, 430, 723, "left lower leg");
-        BodyPart lf = new BodyPart(i_lf, 400, 790, "left foot");
+        BodyPart rua = new BodyPart(i_rua, 405, 420, BodyPart.PART.HEAD,  "right upper arm");
+        BodyPart rla = new BodyPart(i_rla, 390, 533, BodyPart.PART.HEAD,  "right lower arm");
+        BodyPart rh = new BodyPart(i_rh, 396, 605, BodyPart.PART.HEAD,  "right hand");
 
-         BodyPart rul = new BodyPart(i_rul, 490, 608, "right upper leg");
-        BodyPart rll = new BodyPart(i_rll, 492, 725, "right lower leg");
-        BodyPart rf = new BodyPart(i_rf, 500, 790, "right foot");
+        BodyPart lul = new BodyPart(i_lul, 405, 593, BodyPart.PART.HEAD,  "left upper leg");
+        BodyPart lll = new BodyPart(i_lll, 430, 723, BodyPart.PART.HEAD,  "left lower leg");
+        BodyPart lf = new BodyPart(i_lf, 400, 790, BodyPart.PART.HEAD,  "left foot");
 
-        robin_pane.getChildren().add(head.get_img());
-        robin_pane.getChildren().add(body.get_img());
+        BodyPart rul = new BodyPart(i_rul, 490, 608, BodyPart.PART.HEAD,  "right upper leg");
+        BodyPart rll = new BodyPart(i_rll, 492, 725, BodyPart.PART.HEAD,  "right lower leg");
+        BodyPart rf = new BodyPart(i_rf, 500, 790, BodyPart.PART.HEAD,  "right foot");
+        */
+        BodyPart body = new BodyPart(i_body, 425, 400, BodyPart.PART.BODY, "body");
+        BodyPart head = new BodyPart(i_head, -15, -160, BodyPart.PART.HEAD,  "head", 87, 162);
 
-        // left arm
-        robin_pane.getChildren().add(lua.get_img());
-        robin_pane.getChildren().add(lla.get_img());
-        robin_pane.getChildren().add(lh.get_img());
-        // right arm
-        robin_pane.getChildren().add(rua.get_img());
-        robin_pane.getChildren().add(rla.get_img());
-        robin_pane.getChildren().add(rh.get_img());
-        // right leg
-        robin_pane.getChildren().add(rul.get_img());
-        robin_pane.getChildren().add(rll.get_img());
-        robin_pane.getChildren().add(rf.get_img());
-        // left leg
-        robin_pane.getChildren().add(lul.get_img());
-        robin_pane.getChildren().add(lll.get_img());
-        robin_pane.getChildren().add(lf.get_img());
+        BodyPart lua = new BodyPart(i_lua, -15, 30, BodyPart.PART.UPPER_ARM,  "left upper arm", 34, 0);
+        BodyPart lla = new BodyPart(i_lla, -3, 113, BodyPart.PART.LOWER_ARM,  "left lower arm", 10, 0);
+        BodyPart lh = new BodyPart(i_lh, -10, 67, BodyPart.PART.HAND,  "left hand", 19, 0);
+
+        BodyPart rua = new BodyPart(i_rua, 120, 20, BodyPart.PART.UPPER_ARM,  "right upper arm");
+        BodyPart rla = new BodyPart(i_rla, 15, 113, BodyPart.PART.LOWER_ARM,  "right lower arm", 10, 0);
+        BodyPart rh = new BodyPart(i_rh, -4, 67, BodyPart.PART.HAND,  "right hand", 19, 0);
+
+        BodyPart lul = new BodyPart(i_lul, -20, 193, BodyPart.PART.UPPER_LEG,  "left upper leg", 46, 0);
+        BodyPart lll = new BodyPart(i_lll, 25, 130, BodyPart.PART.LOWER_LEG,  "left lower leg", 24, 0);
+        BodyPart lf = new BodyPart(i_lf, -30, 67, BodyPart.PART.FOOT,  "left foot", 24, 0);
+
+        BodyPart rul = new BodyPart(i_rul, 65, 208, BodyPart.PART.UPPER_LEG,  "right upper leg", 35, 0);
+        BodyPart rll = new BodyPart(i_rll, 2, 117, BodyPart.PART.LOWER_LEG,  "right lower leg", 26, 0);
+        BodyPart rf = new BodyPart(i_rf, 8, 65, BodyPart.PART.FOOT,  "right foot", 25, 0);
+
+
+        body.add_children(rua);
+        body.add_children(lua);
+        body.add_children(rul);
+        body.add_children(lul);
+
 
         body.add_children(head);
-        body.add_children(lua);
-        body.add_children(rua);
-        body.add_children(lul);
-        body.add_children(rul);
 
         lua.add_children(lla);
         lla.add_children(lh);
@@ -64,12 +84,87 @@ public class Ragdoll extends Application{
         lll.add_children(lf);
         rul.add_children(rll);
         rll.add_children(rf);
+
+        return body;
+    }
+
+    void init_robin_canvas_manip(Canvas canvas, BodyPart root) {
+        canvas.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> {
+            double x = event.getX();
+            double y = event.getY();
+            BodyPart hit = root.hitTest(x, y);
+            if (hit != null) {
+                selected = hit;
+                //System.out.println("hit " + selected.name);
+                prev_x = event.getX();
+                prev_y = event.getY();
+            }
+        });
+
+      /*  canvas.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
+            double x = event.getX();
+            double y = event.getY();
+            BodyPart hit = root.hitTest(x, y);
+            if (hit != null) {
+                selected = hit;
+                System.out.println("clicked on " + selected.name);
+                prev_x = event.getX();
+                prev_y = event.getY();
+            }
+        }); */
+
+        canvas.addEventFilter(MouseEvent.MOUSE_DRAGGED, event -> {
+            if (selected != null) {
+                double e_x = event.getX();
+                double e_y  = event.getY();
+                double dx = e_x - prev_x;
+                double dy = e_y - prev_y;
+                double distance = Math.sqrt (Math.pow(e_x - prev_x, 2) + Math.pow(e_y - prev_y, 2));
+                BodyPart.ROTATE_DIR x_dir, y_dir;
+                if (prev_x > e_x) {
+                    x_dir = BodyPart.ROTATE_DIR.RIGHT;
+                } else {
+                    x_dir = BodyPart.ROTATE_DIR.LEFT;
+                }
+
+                if (prev_y > e_y) {
+                    y_dir = BodyPart.ROTATE_DIR.UP;
+                } else {
+                    y_dir = BodyPart.ROTATE_DIR.DOWN;
+                }
+
+                double theta = Math.atan(distance);
+                selected.process_move(dx, dy, theta, x_dir, y_dir);
+            }
+
+            draw (canvas, root); // draw in new position
+            prev_x = event.getX();
+            prev_y = event.getY();
+        });
+
+        canvas.addEventFilter(MouseEvent.MOUSE_RELEASED, event -> {
+            selected = null;
+        });
+
     }
 
     @Override
     public void start(Stage stage) {
         stage.setResizable(false);
         stage.setTitle("Ragdoll");
+
+        ToggleButton reset = new ToggleButton("Reset");
+        reset.setOnAction(event -> {
+            if (root != null) {
+                root.reset();
+                draw (canvas, root); // draw in new position
+            }
+        });
+
+        ToggleButton quit = new ToggleButton("Quit");
+        quit.setOnAction(event -> {
+            System.exit(0);
+        });
 
         Image robin_body = new Image (getClass().getResourceAsStream("Christopher Robin/body.png"));
         Image robin_head = new Image (getClass().getResourceAsStream("Christopher Robin/head.png"));
@@ -86,18 +181,29 @@ public class Ragdoll extends Application{
         Image robin_rll = new Image (getClass().getResourceAsStream("Christopher Robin/right lower leg.png"));
         Image robin_rf = new Image (getClass().getResourceAsStream("Christopher Robin/right foot.png"));
 
-
-
-        Pane robin_pane = new Pane();
-
-        init_robin(robin_head, robin_body, robin_lua, robin_lla, robin_lh,
+        BodyPart robin_root = init_robin(robin_head, robin_body, robin_lua, robin_lla, robin_lh,
                 robin_rua, robin_rla, robin_rh,
                 robin_lul, robin_lll, robin_lf,
-                robin_rul, robin_rll, robin_rf,
-                robin_pane);
+                robin_rul, robin_rll, robin_rf);
+        root = robin_root;
 
-        Scene robin_scene = new Scene(robin_pane, 1024, 1280);
-        stage.setScene(robin_scene);
+        Canvas robin_canvas = new Canvas(screen_width, screen_height);
+        canvas = robin_canvas;
+
+        init_robin_canvas_manip(robin_canvas, robin_root);
+        Scene scene = new Scene (new VBox(new HBox(quit, reset), robin_canvas));
+
+        draw(robin_canvas, robin_root);
+
+        stage.setScene(scene);
         stage.show();
     }
+
+    private void draw(Canvas canvas, BodyPart root) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        root.draw(gc);
+    }
+
 }
